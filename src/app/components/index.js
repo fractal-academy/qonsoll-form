00d8142f4b6db71e38f4b209cf730ea48762f31f
sashka2131 <uhucbeucg@gmail.com>
2021-04-06 23:58:30 +0300

    add components QuestionTitle, subtitle, header, editableText, Image and FileUploaders

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,3 +1,11 @@
 export { default as InputForm } from './Input/InputForm'
 export { default as DateTimeInput } from './Input/DateTimeInput'
 export { default as Input } from './Input/Input'
+
+export { default as FileUploader } from './Uploader/FileUploader'
+export { default as ImageUploader } from './Uploader/ImageUploader'
+
+export { default as TextEditable } from './Question/TextEditable'
+export { default as QuestionTitle } from './Question/QuestionTitle'
+export { default as QuestionSubtitle } from './Question/QuestionSubtitle'
+export { default as QuestionHeader } from './Question/QuestionHeader'
--- src/app/index.js
+++ src/app/index.js
@@ -2,41 +2,8 @@ import { Row, Col, Box } from '@qonsoll/react-design'
 import { InputForm } from 'components'
 import { Button, Form, Typography } from 'antd'
 
-const btnProps = {
-  children: 'SUBMIT',
-  type: 'secondary',
-  size: 'small',
-  shape: 'circle'
-}
-const inputProps = {
-  placeholder: 'Input name',
-  type: 'date'
-}
-const btnProps2 = {
-  children: 'Enter your name'
-}
-const inputProps3 = {
-  placeholder: 'Input placeholder'
-}
-
 const App = () => {
-  return (
-    <>
-      <Row m={100}>
-        <Col cw="auto">
-          <InputForm inputProps={inputProps} btn={<Button {...btnProps2} />} />
-        </Col>
-      </Row>
-      <Row m={100}>
-        <Col cw="auto">
-          <InputForm
-            btnProps={{ children: 'Finish' }}
-            inputProps={inputProps3}
-          />
-        </Col>
-      </Row>
-    </>
-  )
+  return <></>
 }
 
 export default App
