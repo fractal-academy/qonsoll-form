a3125cfaaada96a383dd303d157e4536f6212bbd
sashka2131 <uhucbeucg@gmail.com>
2021-04-07 01:21:05 +0300

    add textEditable and question App components

---
+++ src/app/components/index.js
@@ -0,0 +1,4 @@
+export { default as QuestionTitle } from './AppComponents/QuestionTitle'
+export { default as QuestionSubtitle } from './AppComponents/QuestionSubtitle'
+export { default as QuestionHeader } from './AppComponents/QuestionHeader'
+export { default as TextEditable } from './GlobalComponents/TextEditable'
