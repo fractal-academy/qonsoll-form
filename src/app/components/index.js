6c91e7beb83d0a08b6959b083b8fdb61ae01b6bf
sashka2131 <uhucbeucg@gmail.com>
2021-04-07 23:37:33 +0300

    add image and file uploaders, add PropTypes, styling, add new package react-firebase-uploader

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,4 +1,4 @@
-export { default as ImageUploader } from './AppComponents/ImageUploader'
-export { default as FileUploader } from './AppComponents/FileUploader'
 export { default as ImageUploader } from './Uploader/ImageUploader'
 export { default as FileUploader } from './Uploader/FileUploader'
+export { default as IconLabel } from './Uploader/IconLabel'
+export { default as ImageLabel } from './Uploader/ImageLabel'
