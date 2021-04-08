55f827fa7034eec797dcf08bccf22244dfaf712f
sashka2131 <uhucbeucg@gmail.com>
2021-04-08 18:50:32 +0300

    Update index.js
    removed useless component from main index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import App from 'app'
 import { ThemeProvider } from 'styled-components'
 import theme from 'app/config/theme/customTheme'
 import './index.less'
-import DisplayAllComponents from 'app/displayAllComponents'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
-    <DisplayAllComponents />
   </ThemeProvider>,
   document.getElementById('root')
 )
