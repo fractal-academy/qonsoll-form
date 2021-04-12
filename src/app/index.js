9c7520ae9b41a9fa25093bfeb58627e9ee67b212
sashka2131 <uhucbeucg@gmail.com>
2021-04-12 11:22:21 +0300

    add form simple view

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,10 @@
 import { Col, Container, Row } from '@qonsoll/react-design'
 import { Button } from 'antd'
 import 'antd/dist/antd.css'
+import { FormSimpleView } from './domains/Form/components'
 
 const App = (props) => {
-  return <Button>App</Button>
+  return <FormSimpleView />
 }
 
 export default App
