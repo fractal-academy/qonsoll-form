9252bc84ad7556f8ef422a4a64aabd7ba11189f5
5P4C3GR33D <cyber.cuttlefish@gmail.com>
2021-04-13 17:24:33 +0300

    created wraper for condition view and question condition view

---
+++ src/app/components/Card/Card.js
@@ -0,0 +1,55 @@
+import './Card.styles.css'
+import { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Box } from '@qonsoll/react-design'
+// import { useTranslation } from 'react-i18next'
+
+function Card(props) {
+  const { number = '2', children } = props
+
+  // const { ADDITIONAL_DESTRUCTURING_HERE } = user
+
+  // [ADDITIONAL HOOKS]
+  // const { t } = useTranslation('translation')
+  // const { currentLanguage } = t
+
+  // [COMPONENT STATE HOOKS]
+  // const [state, setState] = useState({})
+
+  // [COMPUTED PROPERTIES]
+
+  // [CLEAN FUNCTIONS]
+
+  // [USE_EFFECTS]
+  useEffect(() => {
+    let isComponentMounted = true
+
+    // [EFFECT LOGIC]
+    // write code here...
+    // code sample: isComponentMounted && setState(<your data for state updation>)
+
+    // [CLEAN UP FUNCTION]
+    return () => {
+      // [OTHER CLEAN UP-S (UNSUBSCRIPTIONS)]
+      // write code here...
+
+      // [FINAL CLEAN UP]
+      isComponentMounted = false
+    }
+  }, [])
+
+  return (
+    <Box position="relative">
+      <Box className="hiddenBox"></Box>
+      <Box className="contentBox">{children}</Box>
+      <Box className="numberBox">{number}</Box>
+    </Box>
+  )
+}
+
+Card.propTypes = {
+  number: PropTypes.number,
+  children: PropTypes.node
+}
+
+export default Card
