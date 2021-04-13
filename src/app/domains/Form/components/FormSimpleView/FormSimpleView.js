e1c490b1819c983648b56f7eca5bb3c0b32fda78
sashka2131 <uhucbeucg@gmail.com>
2021-04-13 11:58:32 +0300

    moved style to separate file

--- src/app/domains/Form/components/FormSimpleView/FormSimpleView.js
+++ src/app/domains/Form/components/FormSimpleView/FormSimpleView.js
@@ -1,6 +1,7 @@
 import { Card, Image, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { style } from './FormSimpleView.style'
 // import { useTranslation } from 'react-i18next'
 const { Meta } = Card
 
@@ -39,27 +40,17 @@ function FormSimpleView(props) {
 
   return (
     <Card
-      style={{
-        width: 160,
-        backgroundColor: '#eceff5',
-        borderRadius: '8px'
-      }}
-      bodyStyle={{
-        padding: '0 12px 12px 12px'
-      }}
+      style={style.cardStyles}
+      bodyStyle={style.cardBodyPadding}
       cover={
         <Image
-          style={{
-            height: '110px',
-            padding: '3px',
-            borderRadius: '8px'
-          }}
+          style={style.cardImageStyles}
           alt="Form view"
           src={imageURL}
           preview={false}
         />
       }>
-      <Meta title={title} description={subtitle}></Meta>
+      <Meta title={title} description={subtitle} />
     </Card>
   )
 }
