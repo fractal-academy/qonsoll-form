cb27bb80aacd005021504e172dc6e6d429f60ea8
sashka2131 <uhucbeucg@gmail.com>
2021-04-14 19:57:46 +0300

    rewrite body for FormSimpleView

--- src/app/domains/Form/components/FormSimpleView/FormSimpleView.style.js
+++ src/app/domains/Form/components/FormSimpleView/FormSimpleView.style.js
@@ -1,5 +1,5 @@
 export const styles = {
-  cardStyles: { width: 160, backgroundColor: '#eceff5', borderRadius: '8px' },
-  cardBodyPadding: { padding: '0 12px 12px 12px' },
-  cardImageStyles: { height: '110px', padding: '3px', borderRadius: '8px' }
+  cardStyles: { width: 140, backgroundColor: '#eceff5', borderRadius: '8px' },
+  cardBodyPadding: { padding: '0 8px 8px 8px' },
+  cardImageStyles: { height: '90px', padding: '3px', borderRadius: '8px' }
 }
