e1a03b56413dae766ca0d001cf2252133c5c91e2
sashka2131 <uhucbeucg@gmail.com>
2021-05-26 11:40:34 +0300

    implement logic for File uploader, replace css in js to styled components for Uploader and fix bugs

--- src/app/components/Uploader/IconLabel/IconLabel.js
+++ src/app/components/Uploader/IconLabel/IconLabel.js
@@ -1,27 +1,25 @@
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const IconLabel = (props) => {
-  const { children } = props
-  const AvatarWrapper = styled.label`
+const AvatarWrapper = styled.label`
+  ${({ theme }) => `
     width: 50px;
     height: 50px;
+    font-size:${theme.typography.fontSize.h4};
     border-radius: 50%;
     cursor: pointer;
     justify-content: center;
     display: flex;
     align-items: center;
     position: relative;
-    color: white;
-    //change on prime color in theme
-    border: 3px solid #eff5fd;
-    background-color: #282c34;
-    //change on prime color in theme
-    &:hover {
-      border-color: #eff5fd;
-      background-color: #282c34;
-    }
-  `
+    color: ${theme.color.white.default};
+    background-color: ${theme.color.primary.default};
+  `}
+`
+
+const IconLabel = (props) => {
+  const { children } = props
+
   return <AvatarWrapper>{children}</AvatarWrapper>
 }
 
