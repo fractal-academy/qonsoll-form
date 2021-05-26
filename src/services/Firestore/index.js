5b5217ca7abaf002d4b900d67971228a36c038fe
sashka2131 <uhucbeucg@gmail.com>
2021-05-26 16:39:30 +0300

    removed unnecessary fb functions

--- src/services/Firestore/index.js
+++ src/services/Firestore/index.js
@@ -1,24 +1,16 @@
 import { firestore } from '../Firebase'
 import setData from './setData'
-import getData from './getData'
-import getDataWithFilter from './getDataWithFilter'
 import getCollectionRef from './getCollectionRef'
-import getOrderedData from './getOrderedData'
 import getTimestamp from './getTimestamp'
 import addData from './addData'
 import deleteData from './deleteData'
-import setDocumentListener from './setDocumentListener'
 import updateData from './updateData'
 export {
   updateData,
-  setDocumentListener,
   deleteData,
   addData,
   setData,
-  getData,
   firestore,
-  getDataWithFilter,
   getCollectionRef,
-  getOrderedData,
   getTimestamp
 }
--- src/services/Firestore/setDocumentListener.js
+++
@@ -1,6 +0,0 @@
-import { getCollectionRef } from './'
-
-const setDocumentListener = (collection, document, onSnapshot) => {
-  return getCollectionRef(collection).doc(document).onSnapshot(onSnapshot)
-}
-export default setDocumentListener
