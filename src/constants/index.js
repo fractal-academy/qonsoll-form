5e579037fc25f2e12fd86172e02623555ebd3caa
sashka2131 <uhucbeucg@gmail.com>
2021-05-27 13:40:17 +0300

    fix index file for constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,6 @@ import QUESTION_TYPES from './quetstionType'
 import ANSWER_TYPES from './answerType'
 import DATE_CONDITION_RULES from './dateConditionRules'
 import TEXT_CONDITION_RULES from './planeTextStringConditionRules'
-import ROUTES, { ROUTES_KEYS, ROUTES_VALUES } from './routes'
-import ROUTES_PATHS from './routesPaths'
 import COLLECTIONS from './collection'
 import DEFAULT_IMAGE from './defaultImage'
 
@@ -16,10 +14,6 @@ export {
   ANSWER_TYPES,
   DATE_CONDITION_RULES,
   TEXT_CONDITION_RULES,
-  ROUTES,
-  ROUTES_KEYS,
-  ROUTES_VALUES,
-  ROUTES_PATHS,
   COLLECTIONS,
   DEFAULT_IMAGE
 }
