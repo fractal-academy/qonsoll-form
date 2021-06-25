a2cd796a67cc55e0347c706932f3a26e3b27b3ca
Rostyslav <rostyslavmarchuk440@gmail.com>
2021-06-25 16:21:24 +0300

    added delete question id from all logic jump conditions

--- src/domains/Question/components/QuestionsList/QuestionsList.js
+++ src/domains/Question/components/QuestionsList/QuestionsList.js
@@ -61,6 +61,21 @@ function QuestionsList(props) {
         type: DISPATCH_EVENTS.SET_CURRENT_QUESTION_TO_STATE,
         payload: newCurrentQuestion
       })
+
+    // remove logic jump conditions for all connected questions
+    updatedData.forEach((item) => {
+      let writeToDB = false
+      const editedQuestionConfig = item.questionConfigurations.map((config) => {
+        if (config.redirectQuestion === questionId) {
+          writeToDB = true
+          return { ...config, redirectQuestion: '' }
+        } else return config
+      })
+      writeToDB &&
+        setData(COLLECTIONS.QUESTIONS, item.id, {
+          questionConfigurations: editedQuestionConfig
+        })
+    })
   }
   // [COMPUTED PROPERTIES]
   const dataSource = useMemo(
