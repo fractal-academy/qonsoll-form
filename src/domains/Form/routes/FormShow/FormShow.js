26c62b5bb15410d0c37a6d0696d6e81d005bc5cb
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-07-07 15:36:17 +0300

    merged with library

--- src/domains/Form/routes/FormShow/FormShow.js
+++ src/domains/Form/routes/FormShow/FormShow.js
@@ -52,7 +52,7 @@ function FormShow(props) {
     ) &&
       0) ||
     1
-
+  console.log(data)
   // [COMPONENT STATE HOOKS]
   const [isAnswered, setIsAnswered] = useState(false)
   const [currentSlide, setCurrentSlide] = useState(firstSlideRule)
