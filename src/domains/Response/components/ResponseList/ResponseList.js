88e392db47a135bfe4906a33881ff6acba467d5e
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-07-14 17:45:05 +0300

    added translation

--- src/domains/Response/components/ResponseList/ResponseList.js
+++ src/domains/Response/components/ResponseList/ResponseList.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import { useAnswersContextDispatch } from 'feedback-typeform-app/src/context/Answers'
+import React from 'react'
 import useFunctions from 'feedback-typeform-app/src/hooks/useFunctions'
 import { COLLECTIONS } from 'feedback-typeform-app/src/constants'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
@@ -7,10 +6,11 @@ import { Box, Col } from '@qonsoll/react-design'
 import moment from 'moment'
 import { NumberedCard } from 'feedback-typeform-app/src/components'
 import { EmptyState } from 'feedback-typeform-app/src/domains/Form/components/FormConditionsForm/FormConditionsForm.styles'
+import { useTranslation } from '../../../../context/Translation'
 
 function ResponseList(props) {
   const { formId } = props
-
+  const { emptyStateDescription } = useTranslation()
   // [CUSTOM HOOKS]
   const { getCollectionRef } = useFunctions()
 
@@ -30,14 +30,18 @@ function ResponseList(props) {
           <Col cw={5} key={index} my={2}>
             <NumberedCard number={index + 1}>
               <Box ml={3} my={2}>
-                {moment(item.date.toDate()).format('MMMM Do YYYY, h:mm:ss a')},{' '}
+                {moment(item.date.toDate()).format('MMMM Do YYYY, h:mm:ss a')},
                 {item.user}'s response
               </Box>
             </NumberedCard>
           </Col>
         ))
       ) : (
-        <EmptyState />
+        <EmptyState
+          description={
+            emptyStateDescription || "This form doesn't have any responses yet"
+          }
+        />
       )}
     </>
   )
