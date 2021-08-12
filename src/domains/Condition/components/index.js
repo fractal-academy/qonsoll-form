b4baa277513a254fa049c32954ba1cab3954f78a
aleksandrgolyk <aleksandr.golyk@gmail.com>
2021-08-12 17:09:38 +0300

    removed unused component - ConditionRuleSelect

--- src/domains/Condition/components/index.js
+++ src/domains/Condition/components/index.js
@@ -1,5 +1,4 @@
 export { default as ConditionForm } from './ConditionForm'
-export { default as ConditionRuleSelect } from './ConditionRuleSelect'
 export { default as ModalWithFormConditionsForm } from './ModalWithFormConditionsForm'
 export { default as EndingsSimpleView } from './Endings/EndingsSimpleView'
 export { default as ScoreConditionsAdvancedView } from './ScoreConditionsAdvancedView'
