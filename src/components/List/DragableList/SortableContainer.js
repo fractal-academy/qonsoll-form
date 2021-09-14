489aab3cd4bc98d8a8bc42833189db3b7892a6d9
Your Name <cyber.cuttlefish@gmail.com>
2021-09-14 16:58:14 +0300

    fixed rating color; fixed drag and drop bug

--- src/components/List/DragableList/SortableContainer.js
+++ src/components/List/DragableList/SortableContainer.js
@@ -1,5 +1,9 @@
+import React from 'react'
+import { Box } from '@qonsoll/react-design'
 import { sortableContainer } from 'react-sortable-hoc'
 
-const SortableContainer = sortableContainer(({ children }) => children)
+const SortableContainer = sortableContainer(({ children }) => (
+  <Box>{children}</Box>
+))
 
 export default SortableContainer
